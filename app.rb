02565cb435754f375471cd64c852be15f0802df4
Haala <haalahadithy@gmail.com>
2022-08-10 14:51:54 -0400

    commit tree 25e49810a2dec5ab1c6dc182019eb8ff658b95b5

---
+++ app.rb
@@ -0,0 +1,14 @@
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
+# puts "Hello world!"
+# print "Hello world!" 
+# puts "put a space in here"
+
+# print [1, 3, 5, "i"]
+# puts [1, 3, 5, "i"]
+
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }].pretty_inspect
+# p [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
